248ecf8c43b1714446ba9b15bf382c7db2f58ade
Sheep-s4n <yohann.vdh2008@gmail.com>
2023-10-15 11:09:47 +0200

    maked changes to brightness and added an app launcher module with wofi

--- hypr/scripts/brightness_up.sh
+++ hypr/scripts/brightness_up.sh
@@ -1,2 +1,5 @@
-brightnessctl s +10%
+current_brightness=$(brightnessctl | grep -oP "Current brightness: \d+ \(\K\d+")
+new_brightness=$(($current_brightness + $1))
+echo $new_brightness
+brightnessctl s "$new_brightness%"
 pkill -RTMIN+11 waybar
